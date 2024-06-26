3cbf5083110e9adb2aa7441849f0674ab6c035e8
angelcruz07 <angelcruzsanchez126@gmail.com>
2024-06-26 16:13:37 -0600

    feat: add view product

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,7 +1,45 @@
-export default function () {
+import { titleFont } from '@/config/fonts'
+import { initialData } from '@/seed/seed'
+import { notFound } from 'next/navigation'
+
+interface Props {
+  params: {
+    slug: string
+  }
+}
+
+export default function ({ params }: Props) {
+  const { slug } = params
+
+  const product = initialData.products.find((product) => product.slug === slug)
+
+  if (!product) {
+    notFound()
+  }
+
   return (
-    <div>
-      <h1>Product Page</h1>
+    <div className='mt-5 mb-20 grid md:grid-cols-3 gap-3'>
+      {/* Slideshow */}
+      <div className='col-span-1 md:col-span-2'>
+        <h1>Hola mundo</h1>
+      </div>
+
+      {/* Details */}
+      <div className='col-span-1 px-5'>
+        <h1 className={` ${titleFont.className} antialiased font-bold text-xl`}>
+          {product.title}
+        </h1>
+        <p className='text-lg mb-5'>{product.price}</p>
+        {/* Selector de tallas */}
+
+        {/* Selector de cantidad */}
+
+        <button className-='btn-primary my-5'>Añadir al carrito</button>
+
+        {/* Description */}
+        <h3 className='font-bold text-sm'>Descripcion</h3>
+        <p className='font-light'>{product.description}</p>
+      </div>
     </div>
   )
 }
