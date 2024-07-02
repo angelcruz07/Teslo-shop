f578c9c93e33e03fc37d270ad63f5d5e42317532
angelcruz07 <angelcruzsanchez126@gmail.com>
2024-07-01 19:32:04 -0600

    feat: add view empty

--- src/app/(shop)/empty/page.tsx
+++ src/app/(shop)/empty/page.tsx
@@ -1,7 +1,16 @@
+import { IconShoppingCart } from '@tabler/icons-react'
+import Link from 'next/link'
+
 export default function Empty() {
   return (
-    <div>
-      <h1>Empty Page</h1>
+    <div className='flex justify-center items-center h-[800px]'>
+      <IconShoppingCart stroke={2} size={80} className='mx-5' />
+      <div className='flex flex-col items-center'>
+        <h1 className='text-xl font-semibold'>Tu carrito esta vacio</h1>
+        <Link href='/' className='text-blue-500 text-4xl mt-2'>
+          Regresar
+        </Link>
+      </div>
     </div>
   )
 }
