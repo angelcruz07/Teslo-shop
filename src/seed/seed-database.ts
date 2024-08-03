d2f1a88ccf9f02e3c7137254aaee3fd946154724
angelcruz07 <angelcruzsanchez126@gmail.com>
2024-08-02 19:07:57 -0600

    feat: config db

---
+++ src/seed/seed-database.ts
@@ -0,0 +1,61 @@
+import { initialData } from "./seed";
+import prisma from "../lib/prisma";
+
+async function main() {
+  await Promise.all([
+    prisma.productImage.deleteMany(),
+    prisma.product.deleteMany(),
+    prisma.category.deleteMany(),
+  ]);
+
+  const { categories, products } = initialData;
+
+  const categoriesData = categories.map((name) => ({ name }));
+
+  //Categories
+
+  await prisma.category.createMany({
+    data: categoriesData,
+  });
+
+  const categoriesDB = await prisma.category.findMany();
+
+  const categoriesMap = categoriesDB.reduce(
+    (map, category) => {
+      map[category.name.toLowerCase()] = category.id;
+
+      return map;
+    },
+    {} as Record<string, string>,
+  );
+
+  //products
+
+  products.forEach(async (product) => {
+    const { type, images, ...rest } = product;
+
+    const dbProduct = await prisma.product.create({
+      data: {
+        ...rest,
+        categoryId: categoriesMap[type],
+      },
+    });
+
+    //Images
+
+    const imagesData = images.map((image) => ({
+      url: image,
+      productId: dbProduct.id,
+    }));
+
+    await prisma.productImage.createMany({
+      data: imagesData,
+    });
+  });
+
+  console.log("Seed Executed");
+}
+
+(() => {
+  main();
+})();
