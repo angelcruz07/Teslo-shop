4faa2945072cf3bd2e26c8c6bfe5fd0494a7c5e7
angelcruz07 <angelcruzsanchez126@gmail.com>
2024-08-12 13:20:30 -0600

    fix: solution grid cols

--- src/components/products/product-grid/ProductGridItem.tsx
+++ src/components/products/product-grid/ProductGridItem.tsx
@@ -1,46 +1,39 @@
-'use client'
-import { Product } from '@/interfaces'
-import Image from 'next/image'
-import Link from 'next/link'
-import { useState } from 'react'
+"use client";
+import { Product } from "@/interfaces";
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
 
 interface Props {
-	product: Product
+  product: Product;
 }
 
 export const ProductGridItem = ({ product }: Props) => {
-	const [display, setDisplayImage] = useState(product.images[0])
+  const [display, setDisplayImage] = useState(product.images[0]);
 
-	const handleImage = () => {
-		setDisplayImage(product.images[1])
-	}
-	const mouseEnter = () => {
-		setDisplayImage(product.images[0])
-	}
-
-	return (
-		<div className='rounded-md overflow-hidden fade-in'>
-			<Link href={`/product/${product.slug}`}>
-				<Image
-					src={`/products/${display}`}
-					alt={product.title}
-					className='w-full object-cover rounded'
-					height={500}
-					width={500}
-					onMouseLeave={() => {
-						setDisplayImage(product.images[0])
-					}}
-					onMouseEnter={() => {
-						setDisplayImage(product.images[1])
-					}}
-				/>
-			</Link>
-			<div className='p-4 flex flex-col'>
-				<Link className='hover:text-blue-500' href={`/product/${product.slug}`}>
-					{product.title}
-				</Link>
-				<span className='font-bold'>${product.price}</span>
-			</div>
-		</div>
-	)
-}
+  return (
+    <div className="rounded-md overflow-hidden fade-in">
+      <Link href={`/product/${product.slug}`}>
+        <Image
+          src={`/products/${display}`}
+          alt={product.title}
+          className="w-full object-cover rounded"
+          height={500}
+          width={500}
+          onMouseLeave={() => {
+            setDisplayImage(product.images[0]);
+          }}
+          onMouseEnter={() => {
+            setDisplayImage(product.images[1]);
+          }}
+        />
+      </Link>
+      <div className="p-4 flex flex-col">
+        <Link className="hover:text-blue-500" href={`/product/${product.slug}`}>
+          {product.title}
+        </Link>
+        <span className="font-bold">${product.price}</span>
+      </div>
+    </div>
+  );
+};
